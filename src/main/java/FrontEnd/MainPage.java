e3c489ab4586aea1157f44d5868778d32b1b6b43
Vlad-Enia <vladgio99@gmail.com>
2021-05-24 00:14:11 +0300

    Fixed grades being incorrectly written in table for other students. Adding new students and existing students to groups is fully functional.

--- src/main/java/FrontEnd/MainPage.java
+++ src/main/java/FrontEnd/MainPage.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 
 import javax.swing.*;
 import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -23,101 +24,154 @@ public class MainPage {
     private JPanel mainPagePanel;
     private JPanel configPanel;
     private JComboBox groupComboBox;
-    private JScrollPane contentPanel;
+    private JScrollPane scrollPane;
     private JLabel groupLabel;
     private JTable gradeTable;
+    private JButton addStudentButton;
+    private JPanel contentPanel;
     private Socket socket;
     private PrintWriter out;
     private BufferedReader in;
     private Gson gson;
-    private Object[][] tableData;
-    private String[] tableHeader;
+
 
     public MainPage() {
         groupComboBox.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String group = (String) groupComboBox.getSelectedItem();
+                createTableData(group);
+            }
+        });
 
-                String commandAndJson = "group-student get all {\"name\":\"" + group + "\"}";
-                out.println(commandAndJson);
-                String response = "";
-                try {
-                    response = in.readLine();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                response = response.substring(response.indexOf("["));
-                String[] students = gson.fromJson(response, String[].class);
-                System.out.print("Students from " + group + ": ");
-                for (String student : students)
-                    System.out.print(student + " ");
-                System.out.println();
-
-                commandAndJson = "group-task get all {\"name\":\"" + group + "\"}";
-                out.println(commandAndJson);
-                try {
-                    response = in.readLine();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                response = response.substring(response.indexOf("["));
-                String[] tasks = gson.fromJson(response, String[].class);
-                System.out.print("Tasks for " + group + ": ");
-                for (String task : tasks)
-                    System.out.print(task + " ");
-                System.out.println();
-
-                commandAndJson = "grade get all {\"name\":\"" + group + "\"}";
-                out.println(commandAndJson);
-                try {
-                    response = in.readLine();
-                } catch (IOException ioException) {
-                    ioException.printStackTrace();
-                }
-                response = response.substring(response.indexOf("["));
-                GradeCustomObject[] gradeList = gson.fromJson(response, GradeCustomObject[].class);
-
-
-                String[] tableHeader = new String[tasks.length + 1];
-                tableHeader[0] = "Students \\ Tasks";
-                for (int i = 0; i < tasks.length; i++)
-                    tableHeader[i + 1] = tasks[i];
-
-
-                Object[][] tableData = new Object[students.length][tableHeader.length];
-
-                for (int i = 0; i < students.length; i++)
-                    tableData[i][0] = students[i];
-
-
-                for (int t = 0; t < gradeList.length; t++) {
-                    int j = 0;
-                    boolean ok = false;
-                    for (j = 0; j < tasks.length; j++)
-                        if (gradeList[t].getTask().equals(tasks[j])) {
-                            ok = true;
-                            break;
-                        }
-
-                    if (ok) {
-                        for (int i = 0; i < gradeList[t].getGrade().size(); i++)
-                            tableData[i][j + 1] = gradeList[t].getGrade().get(i);
-                    }
-                }
-
-                for (int i = 0; i < students.length; i++) {
-                    for (int j = 0; j < tableHeader.length; j++)
-                        System.out.print(tableData[i][j] + " ");
-                    System.out.println();
-                }
-                createTableData(tableHeader, tableData);
-
+        addStudentButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String newStudentName = (String) JOptionPane.showInputDialog(
+                        null,
+                        "Student name: ",
+                        "Add Student",
+                        JOptionPane.PLAIN_MESSAGE,
+                        null,
+                        null,
+                        ""
+                );
+                addNewStudent(newStudentName);
             }
         });
     }
 
-    private void createTableData(String[] tableHeader, Object[][] tableData) {
+    private void addNewStudent(String newStudentName) {
+        String commandAndJSON = "register2 " + "{\"username\":\"" + newStudentName + "\"}";
+        out.println(commandAndJSON);
+        String responseCreateStudent = "";
+        try {
+            responseCreateStudent = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String group = (String) groupComboBox.getSelectedItem();
+        String responseGroupStudent = "";
+        commandAndJSON = "group-student add ";
+        commandAndJSON += "{\"groupname\":\"" + group + "\",\"student\":\"" + newStudentName + "\"}";
+        out.println(commandAndJSON);
+        try {
+            responseGroupStudent = in.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (responseGroupStudent.contains("group-student created!"))
+            createTableData(group);
+        else
+            JOptionPane.showMessageDialog(null, "Student \"" + newStudentName + "\" already assigned to group \"" + group + "\"!");
+
+    }
+
+    private void createTableData(String group) {
+        String commandAndJson = "group-student get all {\"name\":\"" + group + "\"}";
+        out.println(commandAndJson);
+        String response = "";
+        try {
+            response = in.readLine();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        response = response.substring(response.indexOf("["));
+        String[] students = gson.fromJson(response, String[].class);
+        System.out.print("Students from " + group + ": ");
+        for (String student : students)
+            System.out.print(student + " ");
+        System.out.println();
+
+        commandAndJson = "group-task get all {\"name\":\"" + group + "\"}";
+        out.println(commandAndJson);
+        try {
+            response = in.readLine();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        response = response.substring(response.indexOf("["));
+        String[] tasks = gson.fromJson(response, String[].class);
+        System.out.print("Tasks for " + group + ": ");
+        for (String task : tasks)
+            System.out.print(task + " ");
+        System.out.println();
+
+        commandAndJson = "grade get all {\"name\":\"" + group + "\"}";
+        out.println(commandAndJson);
+        try {
+            response = in.readLine();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        response = response.substring(response.indexOf("["));
+        GradeCustomObject[] gradeList = gson.fromJson(response, GradeCustomObject[].class);
+
+
+        String[] tableHeader = new String[tasks.length + 1];
+        tableHeader[0] = "Students \\ Tasks";
+        for (int i = 0; i < tasks.length; i++)
+            tableHeader[i + 1] = tasks[i];
+
+
+        Object[][] tableData = new Object[students.length][tableHeader.length];
+
+        for (int i = 0; i < students.length; i++)
+            tableData[i][0] = students[i];
+
+
+        for (int t = 0; t < gradeList.length; t++) {
+            int j = 0;
+            boolean foundTask = false;
+            for (j = 0; j < tasks.length; j++)
+                if (gradeList[t].getTask().equals(tasks[j])) {
+                    foundTask = true;
+                    break;
+                }
+
+            if (foundTask) {
+
+                for (int k = 0; k < gradeList[t].getStudent().size(); k++) {
+                    int i=0;
+                    boolean foundStudent = false;
+                    for(i=0;i< students.length;i++)
+                        if(gradeList[t].getStudent().get(k).equals(students[i]))
+                        {
+                            foundStudent = true;
+                            break;
+                        }
+                    if(foundStudent)
+                        tableData[i][j+1] = gradeList[t].getGrade().get(k);
+                }
+            }
+        }
+
+        for (int i = 0; i < students.length; i++) {
+            for (int j = 0; j < tableHeader.length; j++)
+                System.out.print(tableData[i][j] + " ");
+            System.out.println();
+        }
         TableModel tableModel = new TableModel() {
             @Override
             public int getRowCount() {
@@ -163,7 +217,7 @@ public class MainPage {
                     } else {
                         commandAndJson = "grade add ";
                     }
-                    tableData[rowIndex][columnIndex] = aValue;
+                    tableData[rowIndex][columnIndex] = aux;
                     commandAndJson += "{\"task\":\"" + tableHeader[columnIndex] + "\",\"student\":\"" + tableData[rowIndex][0] + "\",\"grade\":" + aValue + "}";
                     out.println(commandAndJson);
 
@@ -173,9 +227,7 @@ public class MainPage {
                         e.printStackTrace();
                     }
                     JOptionPane.showMessageDialog(null, response);
-                }
-                catch (NumberFormatException e)
-                {
+                } catch (NumberFormatException e) {
                     System.err.println("Invalid input! Must be a number!");
                     JOptionPane.showMessageDialog(null, "Invalid input! Must be a number!");
                 }
@@ -193,8 +245,9 @@ public class MainPage {
             }
         };
         this.gradeTable.setModel(tableModel);
-        this.gradeTable.getTableHeader().setFont(new Font("consolas", Font.BOLD,16));
-        this.gradeTable.getTableHeader().setPreferredSize(new Dimension(-1,30));
+        this.gradeTable.getTableHeader().setFont(new Font("consolas", Font.BOLD, 16));
+        this.gradeTable.getTableHeader().setPreferredSize(new Dimension(-1, 30));
+
     }
 
     private void initFrame(JFrame frame, MainPage MainPage) {
